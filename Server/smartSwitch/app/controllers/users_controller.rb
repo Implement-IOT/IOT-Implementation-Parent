ae6884688a12fce0998ba448e81515726febf79e
akshay <akshay.paliwal@cerner.com>
2018-06-26 21:13:07 +0530

    Adding DeviseTokenAuth gem for token based authentication

--- Server/smartSwitch/app/controllers/users_controller.rb
+++ Server/smartSwitch/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  include DeviseTokenAuth::Concerns::User
   before_action :authenticate_user!
-
+  
 def show
   @user = User.find(params[:id])
 end
--- Server/smartSwitch/config/routes.rb
+++ Server/smartSwitch/config/routes.rb
@@ -1,8 +1,29 @@
 Rails.application.routes.draw do
  
+
+#get '/compliance/privacy_policy.html.erb', to: '/compliance/privacy_policy.html.erb'
+
+ # Stuff
+ # devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
+  #devise_for :users
+  #root to: "home#index"
+
+  # The API part
+  #namespace :api, defaults: {format: :json} do
+  #  scope :v1 do
+  #    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  #    resources :stuff, only: [:index, :show]
+  #  end
+  #end
+  
   root :to => "home#index"
   devise_for :users
   resources :users
+
+
+
+  #get '/switch/:id' , to: "switch#manageSwitch", as: "button"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
